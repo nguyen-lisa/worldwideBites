8f3a1af6d1a87927cb6ef017e426bbfc4678ebc2
Lisa Nguyen <80797200+nguyen-lisa@users.noreply.github.com>
2022-06-08 23:04:29 -0400

    Initial commit for the website. Working nav bar and in progress for a more visually appealing contact section.

---
+++ contactus.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Worldwide Bites </title>
+<link rel="stylesheet" href="mystyle.css">  
+</head>
+    <body>
+        <h1>WORLDWIDE BITES</h1>
+
+        <div class="navbar"> 
+            <a href="worldwidebites.html">Back to main website</a>
+    
+    
+    <form action=”mailto:sugarspiceworldwidebites@gmail.com”
+
+         method=”POST”
+
+         enctype=”multipart/form-data”
+
+         name=”EmailForm”>
+
+         Name:<br>
+
+         <input type=”text” size=”19″ name=”ContactName”><br><br>
+
+         Message:<br> <textarea name=”ContactCommentt” rows=”6″ cols=”20″>
+
+         </textarea><br><br> <input type=”submit” value=”Submit”> </form>
+
+     </body>
+
+</html> 
---
+++ myscripts.js
@@ -0,0 +1,1 @@
+document.write("Page currently unavailable. Please contact sugarspiceworldwidebites@gmail.com for further inquiries.");
